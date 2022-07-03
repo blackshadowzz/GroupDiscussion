6c09ed329777f0659eb37e384746b66471f296a1
blackshadowzz <Rymang66@gmail.com>
2022-07-03 17:42:19 +0700

    Update

--- admin/editData.php
+++ admin/editData.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edit Data Page</title>
-    <?php include_once './bootrap.php'; ?>
-</head>
-<body>
-    <?php include_once "./../assets/navbar.php"; ?>
-    <?php 
+<?php 
 
         require_once './connectDB.php';
         $uname=$phone=$email=$pw="";
@@ -72,6 +61,19 @@
         }
         
     ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit Data Page</title>
+    <?php include_once './bootrap.php'; ?>
+</head>
+<body>
+    <?php include_once "./../assets/navbar.php"; ?>
+    
     <div class="container">
         <form action="" method="post">
             <div class="card">
